7e3e589ef18f06a1a12f61a6205c095fdaae52c5
David Colatti <davidcolatti@gmail.com>
2020-11-19 11:32:10 -0500

    feat: simple method to add 2 nums

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hello_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddNumber(64, 32);
+            Console.WriteLine(result);
+        }
+
+        private static int AddNumber(int numberOne, int numberTwo)
+        {
+            int calculationResult;
+            calculationResult = numberOne + numberTwo;
+            return calculationResult;
+        }
+    }
+}
